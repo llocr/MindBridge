df2ed64beb5bb0857be166e196f7d9cb2547ec49
llocr <36-96@naver.com>
2023-10-08 00:10:13 +0900

    [FEAT] Gender enum class 수정을 통해 mypage.html에서 '성별' 부분이 한글로 표시될 수 있도록 함

--- src/main/java/com/heesue/mindbridge/entity/Gender.java
+++ src/main/java/com/heesue/mindbridge/entity/Gender.java
@@ -1,5 +1,25 @@
 package com.heesue.mindbridge.entity;
 
 public enum Gender {
-    M, F, N
+    M("남자"),F("여자"), N("선택없음");
+
+    private final String value;
+
+    private Gender(String description) {
+        this.value = description;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public static Gender fromValue(String value) {
+        for(Gender gender : values()) {
+            if (gender.getValue().equals(value)) {
+                return gender;
+            }
+        }
+        return null;
+    }
+
 }
