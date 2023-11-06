6253c37b7924b1bfaa159f50e905d3bf9c4c1dea
llocr <36-96@naver.com>
2023-11-07 00:20:23 +0900

    [FEAT] BaseEntity 수정 / lastModifiedDate & createdDate 자동화 설정

--- src/main/java/com/heesue/mindbridge/MindBridgeApplication.java
+++ src/main/java/com/heesue/mindbridge/MindBridgeApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@EnableJpaAuditing //Spring Data JPA의 감사(audit) 기능이 활성화
 public class MindBridgeApplication {
     public static void main(String[] args) {
         SpringApplication.run(MindBridgeApplication.class, args);
--- src/main/java/com/heesue/mindbridge/entity/BaseEntity.java
+++ src/main/java/com/heesue/mindbridge/entity/BaseEntity.java
@@ -1,15 +1,37 @@
 package com.heesue.mindbridge.entity;
 
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
+@Getter
+@Setter
 public abstract class BaseEntity {
+    @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createdDate;
+
+    @LastModifiedDate
     private LocalDateTime lastModifiedDate;
+
     private Long count;
+
     @Enumerated(value = EnumType.STRING)
     private BoardStatus status;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdDate = LocalDateTime.now();
+        this.lastModifiedDate = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.lastModifiedDate = LocalDateTime.now();
+    }
 }
