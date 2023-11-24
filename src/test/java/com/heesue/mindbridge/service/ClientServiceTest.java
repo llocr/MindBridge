3b7e0e52be37528bff6cd97e1e45f8899db81f13
llocr <36-96@naver.com>
2023-11-24 13:36:16 +0900

    [REFACTOR] Client 관련 파일 삭제..!

--- src/test/java/com/heesue/mindbridge/service/ClientServiceTest.java
+++ src/test/java/com/heesue/mindbridge/service/ClientServiceTest.java
@@ -1,90 +1,81 @@
-package com.heesue.mindbridge.service;
-
-import com.heesue.mindbridge.DTO.ClientDTO;
-import com.heesue.mindbridge.entity.Client;
-import com.heesue.mindbridge.repository.ClientRepository;
-import com.heesue.mindbridge.repository.MajorRepository;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.annotation.Commit;
-import org.springframework.transaction.annotation.Transactional;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
-@Transactional
-class ClientServiceTest {
-
-    @Autowired
-    ClientService clientService;
-
-    @Autowired
-    ClientRepository clientRepository;
-
-    @Test
-    public void 회원가입() {
-        //given
-        ClientDTO client = new ClientDTO();
-        client.setId("hello");
-        client.setPassword("12345");
-        client.setName("이희수");
-        client.setStudentNo(2020100539L);
-        client.setEmail("lkj;kj@naver.com");
-        client.setAddress("오동로 32");
-
-        //when
-        String joinId = clientService.join(client);
-
-        //then
-        Client findClient = clientRepository.findById(joinId).get();
-        Assertions.assertEquals(client.getName(),findClient.getName());
-    }
-
-    @Test
-    public void 아이디_중복_확인() throws Exception {
-        //given
-        ClientDTO client = new ClientDTO();
-        client.setId("hello");
-        client.setPassword("12345");
-        client.setName("이희수");
-        client.setStudentNo(2020100539L);
-        client.setEmail("lkj;kj@naver.com");
-        client.setAddress("오동로 32");
-
-        String checkId = "hello";
-
-        //when
-        clientService.join(client);
-        boolean result = clientService.validateDuplicateClientId(checkId);
-
-        //then
-        Assertions.assertEquals(result, true);
-    }
-
-    @Test
-    public void 학번_중복_확인() throws Exception {
-        //given
-        ClientDTO client = new ClientDTO();
-        client.setId("hello");
-        client.setPassword("12345");
-        client.setName("이희수");
-        client.setStudentNo(2020100539L);
-        client.setEmail("lkj;kj@naver.com");
-        client.setAddress("오동로 32");
-
-        Long checkStudentNo = 2020100539L;
-
-        //when
-        clientService.join(client);
-        boolean result = clientService.validateDuplicateClientSudentNo(checkStudentNo);
-
-        //then
-        Assertions.assertEquals(result, true);
-    }
-
-}
+//package com.heesue.mindbridge.service;
+//
+//import com.heesue.mindbridge.DTO.ClientDTO;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//@SpringBootTest
+//@Transactional
+//class ClientServiceTest {
+//
+//    @Autowired
+//    ClientService clientService;
+//
+//    @Autowired
+//    ClientRepository clientRepository;
+//
+//    @Test
+//    public void 회원가입() {
+//        //given
+//        ClientDTO client = new ClientDTO();
+//        client.setId("hello");
+//        client.setPassword("12345");
+//        client.setName("이희수");
+//        client.setStudentNo(2020100539L);
+//        client.setEmail("lkj;kj@naver.com");
+//        client.setAddress("오동로 32");
+//
+//        //when
+//        String joinId = clientService.join(client);
+//
+//        //then
+//        Client findClient = clientRepository.findById(joinId).get();
+//        Assertions.assertEquals(client.getName(),findClient.getName());
+//    }
+//
+//    @Test
+//    public void 아이디_중복_확인() throws Exception {
+//        //given
+//        ClientDTO client = new ClientDTO();
+//        client.setId("hello");
+//        client.setPassword("12345");
+//        client.setName("이희수");
+//        client.setStudentNo(2020100539L);
+//        client.setEmail("lkj;kj@naver.com");
+//        client.setAddress("오동로 32");
+//
+//        String checkId = "hello";
+//
+//        //when
+//        clientService.join(client);
+//        boolean result = clientService.validateDuplicateClientId(checkId);
+//
+//        //then
+//        Assertions.assertEquals(result, true);
+//    }
+//
+//    @Test
+//    public void 학번_중복_확인() throws Exception {
+//        //given
+//        ClientDTO client = new ClientDTO();
+//        client.setId("hello");
+//        client.setPassword("12345");
+//        client.setName("이희수");
+//        client.setStudentNo(2020100539L);
+//        client.setEmail("lkj;kj@naver.com");
+//        client.setAddress("오동로 32");
+//
+//        Long checkStudentNo = 2020100539L;
+//
+//        //when
+//        clientService.join(client);
+//        boolean result = clientService.validateDuplicateClientSudentNo(checkStudentNo);
+//
+//        //then
+//        Assertions.assertEquals(result, true);
+//    }
+//
+//}
