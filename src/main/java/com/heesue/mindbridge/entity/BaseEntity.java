75d13078edd388de890a4f8d7d54bfc306c0a6c6
llocr <36-96@naver.com>
2023-11-24 13:18:44 +0900

    [FEAT] count 값이 증가해도, lastModifiedDate 값이 업데이트 되지 않도록 설정

--- src/main/java/com/heesue/mindbridge/entity/BaseEntity.java
+++ src/main/java/com/heesue/mindbridge/entity/BaseEntity.java
@@ -7,6 +7,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @MappedSuperclass
 @Getter
@@ -19,10 +20,18 @@ public abstract class BaseEntity {
     @LastModifiedDate
     private LocalDateTime lastModifiedDate;
 
+    @Transient
+    private Long previousCount; // count 필드의 이전 값을 추적
+
     private Long count;
 
     @Enumerated(value = EnumType.STRING)
     private BoardStatus status;
+
+    @PostLoad
+    private void onLoad() {
+        this.previousCount = count; // 엔티티가 로드될 때 현재 count 값을 저장
+    }
 
     @PrePersist
     public void prePersist() {
@@ -32,6 +41,12 @@ public abstract class BaseEntity {
 
     @PreUpdate
     public void preUpdate() {
-        this.lastModifiedDate = LocalDateTime.now();
+        if(!Objects.equals(this.count, this.previousCount)) {
+            // count 값이 변경되었으므로 lastModifiedDate를 업데이트하지 않습니다.
+            // 이 로직은 count 값이 변경되었을 때만 호출됩니다.
+        } else {
+            // count 값 외의 다른 변경이 있을 때 lastModifiedDate를 업데이트합니다.
+            this.lastModifiedDate = LocalDateTime.now();
+        }
     }
 }
